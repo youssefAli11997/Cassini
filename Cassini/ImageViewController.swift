a4112f7980b297ed30af84b7db69365c91716e34
youssefali11997 <youssefali6212@gmail.com>
2019-07-09 17:39:45 +0200

    adding an activity indicator view (a spinner)

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -19,6 +19,7 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
         }
     }
     
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var scrollView: UIScrollView! {
         didSet {
@@ -44,12 +45,14 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
     }
  
     func fetchImage() {
+        spinner.startAnimating()
         if let url = imageURL {
             DispatchQueue.global(qos: .userInitiated).async { [weak self] in
                 let urlContents = try? Data(contentsOf: url)
                 DispatchQueue.main.async {
                     if let imageData = urlContents, url == self?.imageURL {
                         self?.imageView.image = UIImage(data: imageData)
+                        self?.spinner.stopAnimating()
                         //to load the image directly from the assets
                         //imageView.image = UIImage(named: "myImage3")!
                     }
