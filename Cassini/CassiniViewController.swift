e99cbb69344a451c2af6d4ea7304c55ba7653ca6
youssefali11997 <youssefali6212@gmail.com>
2019-07-09 15:38:41 +0200

    creating cassini main memu view controller and setting it as initial view controller

---
+++ Cassini/CassiniViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CassiniViewController.swift
+//  Cassini
+//
+//  Created by Owner on 7/9/19.
+//  Copyright © 2019 Owner. All rights reserved.
+//
+
+import UIKit
+
+class CassiniViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ notes.md
@@ -0,0 +1,6 @@
+[master 9a2846e] adding the image in an assts folder and trying out to load it directly from there
+ 7 files changed, 28 insertions(+), 5 deletions(-)
+ rename Cassini/Supporting Files/{Assets.xcassets => Images.xcassets}/AppIcon.appiconset/Contents.json (100%)
+ rename Cassini/Supporting Files/{Assets.xcassets => Images.xcassets}/Contents.json (100%)
+ create mode 100644 Cassini/Supporting Files/Images.xcassets/myImage3.imageset/Contents.json
+ create mode 100644 Cassini/Supporting Files/Images.xcassets/myImage3.imageset/myImage3.jpg
